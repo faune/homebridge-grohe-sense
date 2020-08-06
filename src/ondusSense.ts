ca5a24ebb14eed098bed7ffabc2eb7b6d82e5f5b
Frank Aune <frank.aune@gmail.com>
2020-08-07 00:53:15 +0200

    Prototype of humidity sensor

---
+++ src/ondusSense.ts
@@ -0,0 +1,62 @@
+import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+
+import { OndusPlatform } from './ondus';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class OndusSenseAccessory {
+  private service: Service;
+
+  /**
+   * Ondus Sense constructor for water leakage detectors
+   */
+  constructor(
+    private readonly platform: OndusPlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Grohe')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Ondus-Sense')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
+    // get the Humidity service if it exists, otherwise create a new Humidity service
+    // you can create multiple services for each accessory
+    this.service = this.accessory.getService(this.platform.Service.HumiditySensor) || this.accessory.addService(this.platform.Service.HumiditySensor);
+
+    // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
+    // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
+    // this.accessory.getService('NAME') ?? this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE');
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.senseName);
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Lightbulb
+
+    // register handlers for required characteristics
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
+      .on('get', this.handleCurrentRelativeHumidityGet.bind(this));
+
+  }
+
+  /**
+   * Handle requests to get the current value of the "Current Relative Humidity" characteristic
+   */
+  handleCurrentRelativeHumidityGet(callback) {
+    this.platform.log.debug('Triggered GET CurrentRelativeHumidity');
+
+    // TODO: Fetch number from Ondus web
+
+    // set this to a valid value for CurrentRelativeHumidity
+    const currentValue = 60;
+
+    callback(null, currentValue);
+  }
+
+}
