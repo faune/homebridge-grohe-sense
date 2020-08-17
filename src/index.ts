54eff82645a96194c045578caabc0f589c651d67
Frank Aune <frank.aune@gmail.com>
2020-08-17 21:20:51 +0200

    Fixed warning during startup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { API } from 'homebridge';
 
+import { PLUGIN_NAME } from './settings';
 import { PLATFORM_NAME } from './settings';
 import { OndusPlatform } from './ondusPlatform'; 
 
@@ -7,5 +8,5 @@ import { OndusPlatform } from './ondusPlatform';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, OndusPlatform);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, OndusPlatform);
 }
